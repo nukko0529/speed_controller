260fe8ae3fa32aa13d05f4e368dd9a9460696d0e
nukko0529 <kawausokun58@gmail.com>
2025-09-22 15:01:52 +0900

    アニメーション1種類表示できるように

--- popup.js
+++ popup.js
@@ -2,6 +2,17 @@ const range = document.getElementById("step-range");
 const rangeDisplay = document.getElementById("new-step-value");
 const applyBtn = document.getElementById("apply-btn");
 
+
+const canvas = document.getElementById("otter-canvas");
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+let frame = 0;
+let frameCount = 4;
+let sprite = new Image();
+sprite.src = "img/otter-sleep.png";
+
+
 // 初期読み込み
 chrome.storage.sync.get("step", (data) => {
     if (data.step) step = data.step;
@@ -31,3 +42,18 @@ range.addEventListener("input", () => {
     rangeDisplay.textContent = range.value;
 });
 
+function drawFrame() {
+    let spriteWidth = sprite.width / frameCount;
+    let spriteHeight = sprite.height;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+        sprite,
+        frame * spriteWidth, 0, spriteWidth, spriteHeight,
+        0, 0, canvas.width, canvas.height
+    );
+
+    frame = (frame + 1) % frameCount;
+}
+
+setInterval(drawFrame, 300);
